8c9f9d0f2983f3333f515200c4c48c75a0831bd5
Jay Shi <25982097@qq.com>
2024-03-02 17:09:51 +0800

    rearrange projects

---
+++ StringHashCode/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StringHashCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Original: {"Hello, World!".GetHashCode()}");
+            Console.ReadKey();
+        }
+    }
+}
